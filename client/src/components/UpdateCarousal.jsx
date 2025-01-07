4ab8f172ff1e08fb39fb666370684719e39dfe63
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-01-07 21:01:08 +0530

    feat: added updates carausal

--- client/src/components/UpdateCarousal.jsx
+++ client/src/components/UpdateCarousal.jsx
@@ -27,7 +27,7 @@ const Carousel = () => {
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1.8,
                 },
             },
         ],
