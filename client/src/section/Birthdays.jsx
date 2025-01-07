67bd6eac584ef51012579a32d7d9b2085704f71e
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-01-07 20:22:58 +0530

    feat: added birthday header

---
+++ client/src/section/Birthdays.jsx
@@ -0,0 +1,14 @@
+const Birthdays = () => {
+  return (
+      <section>
+          <div className="flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                  <PiFlowerLotusBold size={35} className="text-red-700" />
+                  <h2 className="text-2xl font-medium">Updates</h2>
+              </div>
+          </div>
+    </section>
+  )
+}
+
+export default Birthdays
