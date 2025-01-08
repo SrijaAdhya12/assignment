4711baae7b26bad22f1f54ab1c0c6c870b70e698
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-01-08 12:05:35 +0530

    fix: logout button

--- client/src/components/LogOut.jsx
+++ client/src/components/LogOut.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LogOut = () => {
     return (
       <div className='p-6 mb-28'>
-        <button className='bg-red-700 text-white p-2 py-3 flex items-center justify-center rounded-md w-full'>LogOut</button>
+        <button className='bg-red-700 text-white font-bold  p-2 py-3 flex items-center tracking-wide justify-center rounded-md w-full'>Log Out</button>
       </div>
     )
 }
