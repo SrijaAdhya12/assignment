c08dff06d60999f7305e877d125826b1bb5c3df3
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-01-08 11:52:38 +0530

    fix: hover animation on bottom bar

--- client/src/components/BottomBar.jsx
+++ client/src/components/BottomBar.jsx
@@ -10,24 +10,24 @@ const BottomNav = () => {
 
     const navItems = [
         { id: 'home', icon: MdHomeFilled, label: 'Home', color: 'text-red-600' },
-        { id: 'leaderboard', icon: MdOutlineLeaderboard, label: 'Leaderboard', color: 'text-gray-600' },
-        { id: 'community', icon: FaPeopleGroup, label: 'Community', color: 'text-gray-600' },
-        { id: 'myarea', icon: RiMapPin2Line, label: 'My Area', color: 'text-gray-600' },
-        { id: 'chats', icon: IoChatbubblesOutline, label: 'Chats', color: 'text-gray-600' },
+        { id: 'leaderboard', icon: MdOutlineLeaderboard, label: 'Leaderboard', color: 'text-red-600' },
+        { id: 'community', icon: FaPeopleGroup, label: 'Community', color: 'text-red-600' },
+        { id: 'myarea', icon: RiMapPin2Line, label: 'My Area', color: 'text-red-600' },
+        { id: 'chats', icon: IoChatbubblesOutline, label: 'Chats', color: 'text-red-600' },
     ];
 
     return (
         <div className="fixed bottom-0 w-full bg-white border-t rounded-t-3xl  border-gray-200">
             <div className="flex justify-between items-center px-5 py-2">
-                {navItems.map(({ id, icon: IconComponent, label, color }) => (
+                {navItems.map(({ id, icon: Icon, label, color }) => (
                     <button
                         key={id}
                         onClick={() => setActiveTab(id)}
                         className="flex flex-col items-center gap-1 px-1 py-2"
                     >
-                        <IconComponent
+                        <Icon
                             size={24}
-                            className={activeTab === id ? color : 'text-gray-400'}
+                            className={activeTab === id ? color : 'text-gray-600 '}
                         />
                         <span
                             className={`text-xs ${activeTab === id ? color : 'text-gray-400'}`}
